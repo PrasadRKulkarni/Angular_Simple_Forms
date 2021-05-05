5bf6605b10ca6d9d1b5d547e2d5143f53b66e843
Prasad Kulkarni <Prasadrkulkarni@gmail.com>
2021-05-05 10:46:20 -0500

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-forms';
+
+  userName: string = '';
+  isChecked: boolean = false;
+
+  onSave(formValue: NgForm) {
+    console.log(formValue.value)
+  }
+
+  reset(formValue: NgForm) {
+    formValue.reset();
+  }
+
 }
